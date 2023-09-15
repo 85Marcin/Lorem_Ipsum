5bfff978da684d1012ab122940567d27af53786e
Marcin <marcinegzmont@gmail.com>
2023-09-15 21:24:59 +0100

    finished- no css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react"
+import { nanoid } from "nanoid"
+import data from "./data"
 function App() {
-  
-  
+  const [count, setCount] = useState(1)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const amount = parseInt(count)
+    setText(data.slice(0, amount))
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          min="1"
+          max="8"
+          step="1"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        ></input>
+        <button className="btn" type="submit">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={nanoid()}>{item}</p>
+        })}
+      </article>
+    </section>
+  )
 }
 
-export default App;
+export default App
